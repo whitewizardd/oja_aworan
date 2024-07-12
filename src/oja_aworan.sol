b7aab400f0c941a0ca6b584c1742ce40fdebec64
whitewizardd <leumasre@gmail.com>
2024-07-12 23:09:38 +0100

    formatted

--- src/oja_aworan.sol
+++ src/oja_aworan.sol
@@ -7,29 +7,18 @@ contract Market_Place {
     address[] public all_created_nft_contracts;
     mapping(address => address[]) public owner_to_contract;
 
-    function create_new_collection(
-        string memory name,
-        string memory symbol
-    ) external {
+    function create_new_collection(string memory name, string memory symbol) external {
         CollectionNFT nft = new CollectionNFT(name, symbol, msg.sender);
         address new_created_nft_contract = address(nft);
         all_created_nft_contracts.push(new_created_nft_contract);
         owner_to_contract[msg.sender].push(new_created_nft_contract);
     }
 
-    function get_all_created_aworan_oja()
-        external
-        view
-        returns (address[] memory created_aworan)
-    {
+    function get_all_created_aworan_oja() external view returns (address[] memory created_aworan) {
         created_aworan = all_created_nft_contracts;
     }
 
-    function get_user_created_aworan_oja()
-        external
-        view
-        returns (address[] memory user_owned_aworan)
-    {
+    function get_user_created_aworan_oja() external view returns (address[] memory user_owned_aworan) {
         user_owned_aworan = owner_to_contract[msg.sender];
     }
 }
